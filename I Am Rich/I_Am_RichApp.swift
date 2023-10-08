f72a2285f3fe3726914221edf9c6c70f4081e992
joy jain <joyshah204@gmial.com>
2023-10-08 15:33:21 +0530

    Initial Commit

---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by JOY JAIN on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
